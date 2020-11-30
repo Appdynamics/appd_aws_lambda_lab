b67cfea7696efaa312659a117e744fe151e1369e
Wayne Brown <wayne.brown@appdynamics.com>
2020-11-30 12:20:29 -0700

    Update handler2.py

--- python/handler2.py
+++ python/handler2.py
@@ -18,6 +18,8 @@ def lambda_function(event, context):
     idx = randint(0, len(objs) - 1)
 
     obj_key = objs[idx]['Key']  
+
+    #TODO: Add in S3 exit call
     with appdynamics.ExitCallContextManager(exit_point_type="CUSTOM", exit_point_sub_type="Amazon S3", identifying_properties={"BUCKET NAME" : os.environ["CANDIDATE_S3_BUCKET"]}) as ec:
         obj = s3_client.get_object(Bucket = os.environ["CANDIDATE_S3_BUCKET"], Key = obj_key)
         
