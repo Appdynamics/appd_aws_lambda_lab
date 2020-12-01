3148b2f9cd9fd65f7ec7ba44b6abb7158b1128a2
Wayne Brown <wayne.brown@appdynamics.com>
2020-11-30 18:33:00 -0700

    Update FrontEndHandler.java

--- java/src/main/java/com/appdynamics/lambda/FrontEndHandler.java
+++ java/src/main/java/com/appdynamics/lambda/FrontEndHandler.java
@@ -1,5 +1,12 @@
 package com.appdynamics.lambda;
 
+//TODO: import AppDynamics tracer classes
+import com.appdynamics.serverless.tracers.aws.api.AppDynamics;
+import com.appdynamics.serverless.tracers.aws.api.Tracer;
+import com.appdynamics.serverless.tracers.aws.api.Transaction;
+import com.appdynamics.serverless.tracers.dependencies.com.google.gson.Gson;
+import com.appdynamics.serverless.tracers.aws.api.ExitCall;
+
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
@@ -23,13 +30,14 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javafaker.Faker;
 
-// TODO: Add in AppDynamics imports
-
 public class FrontEndHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
 
 	private static final Logger LOG = LogManager.getLogger(FrontEndHandler.class);	
 
 	// TODO: Add variables for the tracer and transaction
+	Tracer tracer = null;
+	Transaction txn = null;
+	String correlationHeader = "";
 
 	@Override
 	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
@@ -37,6 +45,20 @@ public class FrontEndHandler implements RequestHandler<Map<String, Object>, ApiG
 		ApiGatewayResponse response;
 
 		// TODO: Add in code to build tracer.
+		tracer = AppDynamics.getTracer(context);
+		
+		if (input.containsKey(Tracer.APPDYNAMICS_TRANSACTION_CORRELATION_HEADER_KEY)) {
+			correlationHeader = input.get(Tracer.APPDYNAMICS_TRANSACTION_CORRELATION_HEADER_KEY).toString();
+		} else {
+			ObjectMapper m = new ObjectMapper();
+			Map<String, Object> headers = m.convertValue(input.get("headers"),new TypeReference<Map<String, Object>>() {});
+			if (headers != null && headers.containsKey(Tracer.APPDYNAMICS_TRANSACTION_CORRELATION_HEADER_KEY)) {
+			  correlationHeader = headers.get(Tracer.APPDYNAMICS_TRANSACTION_CORRELATION_HEADER_KEY).toString();
+			}
+		}
+		
+		txn = tracer.createTransaction(correlationHeader);
+		txn.start();
 
 		String path = input.get("path").toString();
 		Faker faker = new Faker();
@@ -66,9 +88,21 @@ public class FrontEndHandler implements RequestHandler<Map<String, Object>, ApiG
 			String lambda_to_call = context.getFunctionName().replace("lambda-1", "lambda-2");
 			
 			// TODO: Add exit call
+			HashMap<String, String> payload = new HashMap<>();
+			ExitCall lambda_exit_call = null;
+			
+			if (txn != null) {
+			    HashMap<String, String> lambda_props = new HashMap<>();
+			    lambda_props.put("DESTINATION", lambda_to_call);
+			    lambda_props.put("DESTINATION_TYPE", "LAMBDA");
+			    lambda_exit_call = txn.createExitCall("CUSTOM", lambda_props);
+			    String outgoingHeader = lambda_exit_call.getCorrelationHeader();
+			    lambda_exit_call.start();
+			    payload.put(Tracer.APPDYNAMICS_TRANSACTION_CORRELATION_HEADER_KEY, outgoingHeader);
+			}
 	
 			AWSLambda lambdaClient = AWSLambdaClientBuilder.standard().withRegion(System.getenv("AWS_REGION_STR")).build();
-			InvokeRequest request = new InvokeRequest().withFunctionName(lambda_to_call).withPayload("{}");
+			InvokeRequest request = new InvokeRequest().withFunctionName(lambda_to_call).withPayload(new Gson().toJson(payload));
 
 			try {
 				InvokeResult result = lambdaClient.invoke(request);
@@ -84,6 +118,9 @@ public class FrontEndHandler implements RequestHandler<Map<String, Object>, ApiG
 						.build();
 			} catch (Throwable e) {
 				// TODO: Add code to report error for exit call
+				if (lambda_exit_call != null) {
+					lambda_exit_call.reportError(e);
+				}
 
 				Map<String, Object> error_map = new HashMap<String, Object>();
 				error_map.put("error_msg", e.getMessage());
@@ -93,7 +130,10 @@ public class FrontEndHandler implements RequestHandler<Map<String, Object>, ApiG
 						.build();
 			}
 
-			// TODO: Add code to end exit call			
+			// TODO: Add code to end exit call	
+			if (lambda_exit_call != null) {
+			    lambda_exit_call.stop();
+			}
 
 		} else {
 
@@ -111,6 +151,9 @@ public class FrontEndHandler implements RequestHandler<Map<String, Object>, ApiG
 		}
 
 		// TODO: Add code to end transaction
+		if (txn != null) {
+			txn.stop();
+		}
 
 		return response;
 
